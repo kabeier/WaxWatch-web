1138b8fbf780ebbc67ebe0a5a2d0764a618299bd
kabeier <kabeier@gmail.com>
2026-03-12 17:29:23 -0400

    Format health API route with Prettier

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -3,11 +3,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { withApiRequestLogging } from "@/lib/server/request-logging";
 
 export default withApiRequestLogging(function health(_req: NextApiRequest, res: NextApiResponse) {
-  res
-    .status(200)
-    .json({
-      status: "ok",
-      app: env.NEXT_PUBLIC_APP_NAME,
-      release: env.NEXT_PUBLIC_RELEASE_VERSION,
-    });
+  res.status(200).json({
+    status: "ok",
+    app: env.NEXT_PUBLIC_APP_NAME,
+    release: env.NEXT_PUBLIC_RELEASE_VERSION,
+  });
 });
